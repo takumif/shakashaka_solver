f1379b6e4360f4225700ee4e4d74958bd36bf0e2
Takumi Fujimoto <takumi@takumium.com>
2016-04-04 23:46:04 -0400

    Add JS files; modify .gitignore to include JS files

---
+++ javascripts/board_io.js
@@ -0,0 +1,25 @@
+/// <reference path="board.ts" />
+/// <reference path="../typings/main.d.ts" />
+function drawBoard(board) {
+    $(".board").empty();
+    if (board.length === 0) {
+        return;
+    }
+    for (var row = 0; row < board.length; row++) {
+        var rowDiv = $("<div></div>", {
+            id: "row" + String(row),
+            class: "row"
+        });
+        for (var col = 0; col < board[0].length; col++) {
+            var squareDiv = $("<div></div>", {
+                id: "r" + String(row) + "c" + String(col),
+                row: row,
+                col: col,
+                class: "square",
+                squareType: squareToString(board[row][col])
+            });
+            rowDiv.append(squareDiv);
+        }
+        $(".board").append(rowDiv);
+    }
+}
---
+++ javascripts/script.js
@@ -0,0 +1,6 @@
+/// <reference path="../typings/main.d.ts" />
+/// <reference path="board.ts" />
+/// <reference path="board_io.ts" />
+$(function () {
+    drawBoard(initBoard(10, 10));
+});
