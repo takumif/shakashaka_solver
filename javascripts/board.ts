0d96b62e630df63a2b5db5d10b25086bb57e7b42
ChaseVendetta <wakendarky@gmail.com>
2016-04-09 09:52:39 -0400

    Random Board function added

--- javascripts/board.ts
+++ javascripts/board.ts
@@ -51,3 +51,20 @@ function squareToString(type: Square): string {
 function stringToSquare(name: string): Square {
     return Square[name];
 }
+
+function randomBoard(boardLen:number): Square[][] {
+    // Lame input.
+    if (boardLen <= 1) return null;
+    
+    var board: Square[][] = new Array(boardLen);
+    
+    for (var i = 0; i < boardLen; i++){
+        board[i] = new Array(boardLen);
+        for (var j = 0; j < boardLen; j++){
+            // A random type from 6 to 11.
+            board[i][j] = Math.floor(Math.random() * 6) + 6;
+        }
+    }
+    
+    return board;
+}
