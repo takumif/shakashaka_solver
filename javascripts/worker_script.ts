838a0bb1b6c97f5d03bf687faa333f56bc387d35
Takumi Fujimoto <takumi@takumium.com>
2016-05-01 21:08:18 -0400

    Start working on web workers

---
+++ javascripts/worker_script.ts
@@ -0,0 +1,15 @@
+/// <reference path="par_solver.ts" />
+importScripts(
+    "board.js",
+    "solver.js",
+    "par_solver.js",
+    "solver_helper.js"
+);
+
+onmessage = function(e: MessageEvent): void {
+    var message = {
+        workId: e.data.workId,
+        result: parSolveByGuessing(e.data.work)
+    };
+    postMessage(message);
+}
