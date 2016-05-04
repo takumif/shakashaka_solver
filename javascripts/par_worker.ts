1186a12b96e72bd1164cd31b217661ba51236b9a
Takumi Fujimoto <takumi@takumium.com>
2016-05-03 20:27:03 -0400

    Make parallel worker use a queue

--- javascripts/par_worker.ts
+++ javascripts/par_worker.ts
@@ -1,36 +1,52 @@
 class ParallelWorker {
     private static workerScript = "javascripts/worker_script.js";
-    private workQueue: (() => any)[];
     private worker: Worker;
     private busy: boolean;
     private totalWorkCount: number;
-    private callbacks: { [id: number]: (result: any) => void };
+    private nextWork: number;
+    private works: { [id: number]: { work: any, callback: (result: any) => void} };
     
     constructor() {
-        this.workQueue = [];
         this.busy = false;
         this.initWorker();
         this.totalWorkCount = 0;
-        this.callbacks = {};
+        this.nextWork = 0;
+        this.works = {};
     }
     
     submitWork(work: any, callback: (result: any) => void): void {
-        if (this.busy) {
-            this.workQueue.push(work);
-        } else {
-            var workId = this.totalWorkCount++;
-            this.callbacks[workId] = callback;
+        var workId = this.totalWorkCount++;
+        this.works[workId] = { work: work, callback: callback };
+        if (!this.busy) {
+            this.busy = true;
             this.worker.postMessage({ work: work, workId: workId });
+        }
+    }
+    
+    getWorkCount(): number {
+        if (this.busy) {
+            return this.totalWorkCount - this.nextWork + 1;
+        } else {
+            return this.totalWorkCount - this.nextWork;
         }
     }
     
     private initWorker(): void {
         this.worker = new Worker(ParallelWorker.workerScript);
         this.worker.onmessage = (e: MessageEvent): void => {
-            console.log(e.data.workId);
-            console.log(this.callbacks);
-            var callback = this.callbacks[e.data.workId];
-            callback(e.data.result);
+            this.processResult(e.data.result, e.data.workId);
+        }
+    }
+    
+    private processResult(result: any, workId: number) {
+        var callback = this.works[workId].callback;
+        callback(result);
+        if (this.nextWork < this.totalWorkCount) {
+            var work = this.works[this.nextWork].work;
+            this.worker.postMessage({ work: work, workId: this.nextWork });
+            this.nextWork++;
+        } else {
+            this.busy = false;
         }
     }
 }
